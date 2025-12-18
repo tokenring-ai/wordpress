98bb74785a130ce897322003f2c44be4eb393a57
Mark Dierolf <mdierolf@cloudquote.io>
2025-12-18 13:53:39 -0800

    Extract and modularize plugin logic into `plugin.ts`

---
+++ plugin.ts
@@ -0,0 +1,40 @@
+import TokenRingApp, { TokenRingPlugin } from "@tokenring-ai/app";
+import {BlogConfigSchema, BlogService} from "@tokenring-ai/blog";
+import {CDNConfigSchema, CDNService} from "@tokenring-ai/cdn";
+import packageJSON from './package.json' with {type: 'json'};
+import WordPressBlogProvider, {WordPressBlogProviderOptionsSchema} from "./WordPressBlogProvider.js";
+import WordPressCDNProvider, {WordPressCDNProviderOptionsSchema} from "./WordPressCDNProvider.js";
+
+
+export default {
+  name: packageJSON.name,
+  version: packageJSON.version,
+  description: packageJSON.description,
+  install(app: TokenRingApp) {
+    const cdnConfig = app.getConfigSlice("cdn", CDNConfigSchema);
+
+    if (cdnConfig) {
+      app.services.waitForItemByType(CDNService, cdnService => {
+        for (const name in cdnConfig.providers) {
+          const provider = cdnConfig.providers[name];
+          if (provider.type === "wordpress") {
+            cdnService.registerProvider(name, new WordPressCDNProvider(WordPressCDNProviderOptionsSchema.parse(provider)));
+          }
+        }
+      });
+    }
+
+    const blogConfig = app.getConfigSlice("blog", BlogConfigSchema);
+
+    if (blogConfig) {
+      app.services.waitForItemByType(BlogService, blogService => {
+        for (const name in blogConfig.providers) {
+          const provider = blogConfig.providers[name];
+          if (provider.type === "wordpress") {
+            blogService.registerBlog(name, new WordPressBlogProvider(WordPressBlogProviderOptionsSchema.parse(provider)));
+          }
+        }
+      });
+    }
+  },
+} as TokenRingPlugin;
